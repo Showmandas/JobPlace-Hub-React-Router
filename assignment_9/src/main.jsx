ddbcf756a10c45237cbd0f5d2a4c17b6860d8d7f
Showman_Das <Showmandas3216@gmail.com>
2023-04-10 09:00:42 +0600

    react router set up and create components

--- assignment_9/src/main.jsx
+++ assignment_9/src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './components/Layout';
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path:'/',
+        element:<Home />
+      }
+    ]
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
