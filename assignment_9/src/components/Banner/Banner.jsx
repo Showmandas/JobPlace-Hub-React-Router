3874c936d5a73ede9d2a933f923abf9477f13c0b
Showman_Das <Showmandas3216@gmail.com>
2023-04-10 16:29:08 +0600

    banner section done

---
+++ assignment_9/src/components/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Banner.css'
+const Banner = () => {
+    return (
+        <div className='container'>
+            <div className="row justify-center align-items-center">
+                <div className="col-lg-6 col-12">
+                    <h1 className='fw-1 w-75' id='heading'>One Step Closer To Your <span>Dream Job</span></h1>
+                    <p className='text-secondary fs-6'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                    <button className='btn text-white fw-semibold' id='btn-start'>Get Started</button>
+                </div>
+                <div className='col-lg-6 col-12'>
+                    <img src="man.png" className='img-fluid' alt="man picture" />
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Banner;
