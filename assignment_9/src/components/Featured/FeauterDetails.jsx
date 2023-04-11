1c6dd7b67391767c637894b269815a39ffab7dd5
Showman_Das <Showmandas3216@gmail.com>
2023-04-11 18:01:51 +0600

    add icons and edit some style in details page

--- assignment_9/src/components/Featured/FeauterDetails.jsx
+++ assignment_9/src/components/Featured/FeauterDetails.jsx
@@ -14,7 +14,7 @@ const FeauterDetails = () => {
   return (
     <div>
       {/* <h3>Details</h3> */}
-      <div className="row py-5 mt-5 gx-4">
+      <div className="row py-5 mt-4 gx-4">
         <div className="col-lg-7 col-12">
           <p>
             <span className="fw-semibold">Job Description: </span>
@@ -38,16 +38,17 @@ const FeauterDetails = () => {
             <div className="card-body">
                 <h5>Job Details</h5>
                 <hr />
-              <p className="card-title"><span className="fw-semibold">Salary : </span>{jobDetail.salary}</p>
-              <p className="card-title"><span className="fw-semibold">Job Title : </span> {jobDetail.title}</p>
+              <p className="card-title"><img src="../../../public/images/Frame.png"/><span className="fw-semibold px-2">Salary : </span>{jobDetail.salary}</p>
+              <p className="card-title"><img src="../../../public/images/Frame (1).png"/><span className="fw-semibold px-2">Job Title : </span> {jobDetail.title}</p>
              <h5 className="mt-5">Contact Information</h5>
              <hr />
-             <p><span className="fw-semibold">Phone : </span>{jobDetail.phone}</p>
-             <p><span className="fw-semibold">Email : </span>{jobDetail.email}</p>
-             <p><span className="fw-semibold">Address :</span>{jobDetail.address}</p>
+             <p><img src="../../../public/images/Frame (2).png"/><span className="fw-semibold px-2">Phone : </span>{jobDetail.phone}</p>
+             <p><img src="../../../public/images/Frame (3).png"/><span className="fw-semibold px-2">Email : </span>{jobDetail.email}</p>
+             <p><img src="../../../public/images/Frame (4).png"/><span className="fw-semibold px-2">Address :</span>{jobDetail.address}</p>
             </div>
-          <button className="btn common">Apply Now</button>
           </div>
+          <button className="btn common w-100 mt-3">Apply Now</button>
+
         </div>
       </div>
     </div>
