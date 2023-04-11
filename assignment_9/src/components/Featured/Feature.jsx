459fd4c1224dee0575f95d54f77057a267f391ec
Showman_Das <Showmandas3216@gmail.com>
2023-04-11 14:46:55 +0600

    see all jobs part complete

--- assignment_9/src/components/Featured/Feature.jsx
+++ assignment_9/src/components/Featured/Feature.jsx
@@ -4,6 +4,7 @@ import FeatureCard from './FeatureCard';
 
 const Feature = () => {
     const [featureData,setFeatureData]=useState([]);
+    const[seeAllJobs,setSeeAllJobs]=useState(false);
 
     useEffect(()=>{
         fetch('Features.json')
@@ -17,9 +18,14 @@ const Feature = () => {
             <p className='text-center text-body-secondary fs-5 fw-semibold'>Explore thousands of job opportunities with all the information you need. Its your future</p>
              <div className="row gy-5 mt-5">
                 {
-                    featureData.slice(0,4).map(feature=><FeatureCard feature={feature} key={feature.id}/>)
+                    featureData.slice(0,seeAllJobs ? 6 : 4).map(feature=><FeatureCard feature={feature} key={feature.id}/>)
                 }
 
+             </div>
+             <div className='d-flex justify-content-center align-items-center mt-5 py-4'>
+             {
+                !seeAllJobs && <button className='common btn justify-content-center' onClick={()=>setSeeAllJobs(true)}>See All Jobs</button>
+             }
              </div>
         </div>
     );
