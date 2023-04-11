5f438eda51c0ee235058b2935d27892b94aafd03
Showman_Das <Showmandas3216@gmail.com>
2023-04-11 10:30:36 +0600

    fontAwesome cdn added to the project

---
+++ assignment_9/src/components/Category/Category.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Category = () => {
+    return (
+        <div>
+            <h1>Job Category List</h1>
+            <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+        </div>
+    );
+};
+
+export default Category;
